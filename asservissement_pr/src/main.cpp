95c19cba9e09c9409c877e3bef606b04982e8504
I3at57 <75752042+I3at57@users.noreply.github.com>
2021-06-25 14:54:23 +0200

    MAJ_leo

--- asservissement_pr/src/main.cpp
+++ asservissement_pr/src/main.cpp
@@ -1,3 +1,4 @@
+
 /*
  * \file asservissement_pr.ino
  * \brief Holds the arduino setup and loop functions, which make the asserv run.
@@ -5,7 +6,9 @@
 
 
 //#include "SimpleTimer.h"
+#include <Arduino.h>
 #include <SimpleTimer.h>
+#include <Encoder.h>
 
 #include "parameters.h"
 #include "goalList.h"
@@ -14,7 +17,6 @@
 #include "goal.h"
 
 
-#include <Encoder.h>
 
 Encoder rightEnc = Encoder(A_RIGHT, B_RIGHT);
 Encoder leftEnc = Encoder(A_LEFT, B_LEFT);
@@ -23,47 +25,6 @@ extern Collisions collisions;
 extern GoalList goalList;
 
 SimpleTimer timer = SimpleTimer();
-
-void setup() {
-   Serial.begin(9600);
-   timer.setInterval(TIMER_MS, asservLoop);
-   fillGoals();
-   
-}
-
-void loop() {
-     
-        
-        /*static bool a = true;
-        if (a){
-          timer.setInterval(TIMER_MS, asservLoop);
-          fillGoals();
-          a = false;
-        }*/
-       
-        //Debug coders
-        /*digitalWrite(FORWARD_LEFT, HIGH);
-        digitalWrite(BACKWARDS_LEFT, LOW);
-        analogWrite(PWM_LEFT, 100);
-
-        digitalWrite(FORWARD_RIGHT, HIGH);
-        digitalWrite(BACKWARDS_RIGHT, LOW);
-        analogWrite(PWM_RIGHT, 100);
-        //int leftTicks = leftEnc.read();
-        //int rightTicks = - rightEnc.read();
-        Serial.println(leftEnc.read());
-        Serial.println(- rightEnc.read());
-        delay(500);
-        return;*/
-
-  
-  timer.run();
-  Serial.println("loop");
-  //delay(10);
-  //fillGoals();
-  
-}
-
 
 void fillGoals() {
     // TODO add goal dynamically with ros or custom serial / CAN protocol
@@ -88,3 +49,43 @@ void asservLoop() {
     Serial.println("asservLoop");
     
 }
+void setup() {
+    Serial.begin(9600);
+    timer.setInterval(TIMER_MS, asservLoop);
+    fillGoals();
+}
+
+void loop() {
+         
+        /*static bool a = true;
+        if (a){
+          timer.setInterval(TIMER_MS, asservLoop);
+          fillGoals();
+          a = false;
+        }*/
+       
+        //Debug coders
+        digitalWrite(FORWARD_LEFT, HIGH);
+        digitalWrite(BACKWARDS_LEFT, LOW);
+        analogWrite(PWM_LEFT, 100);
+
+        digitalWrite(FORWARD_RIGHT, HIGH);
+        digitalWrite(BACKWARDS_RIGHT, LOW);
+        analogWrite(PWM_RIGHT, 100);
+        //int leftTicks = leftEnc.read();
+        //int rightTicks = - rightEnc.read();
+        Serial.println(leftEnc.read());
+        Serial.println(- rightEnc.read());
+        delay(500);
+        return;
+        
+
+  
+  timer.run();
+  Serial.println("loop");
+  //delay(10);
+  //fillGoals();
+}
+
+
+
